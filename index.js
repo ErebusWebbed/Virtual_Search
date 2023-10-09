8dde7ca843862db4b52d80bd7c8b1b0426d8895e
ErebusWebbed <121348804+ErebusWebbed@users.noreply.github.com>
2023-10-09 21:19:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Find your <br>desired products<br> in a <span>CLICK!<span>"],
+    typeSpeed: 50,
+    loop: false
+})
+
+function scrollBottom() {
+    window.scrollTo(0, document.body.scrollHeight);
+}
